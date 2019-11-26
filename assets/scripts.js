a03686389295499906157eb165e66d1bed6de8af
vdakshin <vijay.dakshinamoorthy@gmail.com>
2019-11-26 11:38:16 -0500

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,38 +1,3 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
-
-$(function() {
-    $("#contactForm").submit(function(e) {
-      e.preventDefault();
-      $.ajax({
-        url: "//formspree.io/vijay.dakshinamoorthy@gmail.com", 
-        method: "POST",
-        data: $(this).serialize(),
-        dataType: "json",
-        success: function(data){
-          // Success message
-          $('#success').html("<div class='alert alert-success'>");
-          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-              .append("</button>");
-          $('#success > .alert-success')
-              .append("<strong>Your message has been sent. </strong>");
-          $('#success > .alert-success')
-              .append('</div>');
-
-          //clear all fields
-          $('#contactForm').trigger("reset");
-        },
-        error: function(){
-          // Fail message
-          $('#success').html("<div class='alert alert-danger'>");
-          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-              .append("</button>");
-          $('#success > .alert-danger').append("<strong>Sorry it seems that my mail server is not responding. Please try again later!");
-          $('#success > .alert-danger').append('</div>');
-          //clear all fields
-          $('#contactForm').trigger("reset");
-        }
-      });
-    });
-});
